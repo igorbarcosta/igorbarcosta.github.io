e24fb8f631b5cbf3f7d44a0811c179ac3190c4e6
Igor Barbosa Da Costa <igormago@pop-os.local>
2025-05-25 23:54:57 -0300

    Corrige base para GitHub Pages e atualiza site

--- src/settings.ts
+++ src/settings.ts
@@ -34,7 +34,7 @@ export const template = {
 	darkTheme: 'dark', // Select one of the Daisy UI Themes or create your own
 	excerptLength: 200,
 	postPerPage: 5,
-    base: '/' // Repository name starting with /
+    base: '' // Repository name starting with /
 }
 
 export const seo = {
